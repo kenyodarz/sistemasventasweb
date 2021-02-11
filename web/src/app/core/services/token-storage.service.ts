b97ecaae5526723db029f5a715c578798e5d4783
Jorge Mina <minajorge@hotmail.com>
2021-02-11 11:43:02 -0500

    Sistema de Login Implementado correctamente

---
+++ web/src/app/core/services/token-storage.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+const TOKEN_KEY = 'auth-token'
+const USER_KEY = 'auth-user'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenStorageService {
+  constructor() {}
+
+  signOut() {
+    window.sessionStorage.clear();
+  }
+
+  guardarToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  obtenerToken(): string {
+    return window.sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  guardarUsuario(user: any): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  obtenerUsuario() {
+    return JSON.parse(sessionStorage.getItem(USER_KEY));
+  }
+}
---
+++ web/src/app/pages/resume/resume-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from 'src/app/guards/login.guard';
+import { HomeComponent } from './components/home/home.component';
+import { ResumeComponent } from './resume.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+    canActivate: [LoginGuard],
+  },
+  {
+    path: '',
+    component: ResumeComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [LoginGuard],
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ResumeRoutingModule { }
