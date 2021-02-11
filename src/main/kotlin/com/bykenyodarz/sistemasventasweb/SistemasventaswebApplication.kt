89b70e0f6f832bdb5fcda32f593d201268c6b9b5
Jorge Mina <minajorge@hotmail.com>
2021-02-11 07:29:35 -0500

    Initial commit

---
+++ src/main/kotlin/com/bykenyodarz/sistemasventasweb/SistemasventaswebApplication.kt
@@ -0,0 +1,11 @@
+package com.bykenyodarz.sistemasventasweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SistemasventaswebApplication
+
+fun main(args: Array<String>) {
+    runApplication<SistemasventaswebApplication>(*args)
+}
