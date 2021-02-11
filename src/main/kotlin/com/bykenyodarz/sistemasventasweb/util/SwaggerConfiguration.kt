4c07478a9ff8241a9535edddbeac9efb0183021b
Jorge Mina <minajorge@hotmail.com>
2021-02-11 07:44:46 -0500

    Swagger Configuration

---
+++ src/main/kotlin/com/bykenyodarz/sistemasventasweb/util/SwaggerConfiguration.kt
@@ -0,0 +1,30 @@
+package com.bykenyodarz.sistemasventasweb.util
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfiguration {
+    fun documentation(): Docket? {
+        return Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors.withClassAnnotation(RestController::class.java))
+            .paths(PathSelectors.any())
+            .build()
+    }
+
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+    }
+}
