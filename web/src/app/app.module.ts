f9c3a31d3ec0132fcbeaef64892d2daa1cc2dce3
Jorge Mina <minajorge@hotmail.com>
2021-02-11 09:23:33 -0500

    rutas implementadas

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,18 +1,37 @@
+/* Angular */
 import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+/* Modules */
+import { AppRoutingModule } from './app-routing.module';
+import { PrimengModule } from './shared/primeng.module';
+/* Providers / Services */
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+/* Components */
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PrimengModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    MessageService,
+    DialogService,
+    ConfirmationService,
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
